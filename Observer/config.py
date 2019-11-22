084ebe97c911cda8934745896b0aa9f7b41cf189
Linards Kalvāns <linards.kalvans@gmail.com>
2019-11-22 21:18:18 +0200

    Default observation interval is 60s

--- Observer/config.py
+++ Observer/config.py
@@ -10,4 +10,4 @@ class Config(object):
     DATA_SOURCE_ID = os.environ.get('DATA_SOURCE_ID')
     DATA_SOURCE = os.environ.get('DATA_SOURCE')
     MAX_QUEUE_ITEMS = os.environ.get('MAX_QUEUE_ITEMS', 100)
-    OBSERVATION_INTERVAL = os.environ.get('OBSERVATION_INTERVAL', 300)
+    OBSERVATION_INTERVAL = os.environ.get('OBSERVATION_INTERVAL', 60)
