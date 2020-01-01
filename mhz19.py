1294e8d5381aef08830d7152ec6c36f55ef9e02a
Linards Kalvāns <linards.kalvans@twino.eu>
2020-01-01 14:28:29 +0200

    Use transistor relay for restarting MH Z19 on failure

--- mhz19.py
+++ mhz19.py
@@ -2,6 +2,10 @@ import subprocess
 import json
 import mh_z19
 from Observer.Observer import Observer
+import RPi.GPIO as GPIO
+import time
+
+RELAY_PIN = 23
 
 class ObserverMHZ19(Observer):
     def __init__(self, name, session = None, *args, **kwargs):
@@ -10,10 +14,19 @@ class ObserverMHZ19(Observer):
         }
         super(ObserverMHZ19, self).__init__(session = session, name = name, *args, **kwargs)
         mh_z19.abc_off()
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(RELAY_PIN, GPIO.OUT)
+        GPIO.output(RELAY_PIN, GPIO.HIGH)
 
     def observe(self):
 #        raw_data = subprocess.run(['python', '-m', 'mh_z19'], stdout=subprocess.PIPE)
-        co2 = mh_z19.read().get('co2', None)
+        try:
+            co2 = mh_z19.read().get('co2', None)
+        except:
+            GPIO.output(RELAY_PIN, GPIO.LOW)
+            time.sleep(10)
+            GPIO.output(RELAY_PIN, GPIO.HIGH)
+            co2 = mh_z19.read().get('co2', None)
         self.logger.info('CO2 = %d ppm' % (co2))
         return {
             'mhz19_co2': co2
