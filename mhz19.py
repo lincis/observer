76f51120d91252f08f76facdde0c4b45221f7ed4
Linards Kalvāns <linards.kalvans@twino.eu>
2020-02-01 22:12:45 +0200

    Read from serial directly

--- mhz19.py
+++ mhz19.py
@@ -1,10 +1,44 @@
 import subprocess
 import json
-import mh_z19
 from Observer.Observer import Observer
-import RPi.GPIO as GPIO
 import time
+import serial
+import time
+import traceback
+import getrpimodel
+import struct
+import platform
+import argparse
+import sys
+import json
+import os.path
 
+# setting
+version = "0.3.9"
+pimodel        = getrpimodel.model
+pimodel_strict = getrpimodel.model_strict()
+
+if os.path.exists('/dev/serial0'):
+  partial_serial_dev = 'serial0'
+elif pimodel == "3 Model B" or pimodel_strict == "Zero W":
+  partial_serial_dev = 'ttyS0'
+else:
+  partial_serial_dev = 'ttyAMA0'
+
+serial_dev = '/dev/%s' % partial_serial_dev
+stop_getty = 'sudo systemctl stop serial-getty@%s.service' % partial_serial_dev
+start_getty = 'sudo systemctl start serial-getty@%s.service' % partial_serial_dev
+
+# major version of running python
+p_ver = platform.python_version_tuple()[0]
+
+def connect_serial():
+    return serial.Serial(serial_dev,
+                        baudrate=9600,
+                        bytesize=serial.EIGHTBITS,
+                        parity=serial.PARITY_NONE,
+                        stopbits=serial.STOPBITS_ONE,
+                        timeout=1.0)
 RELAY_PIN = 23
 
 class ObserverMHZ19(Observer):
@@ -13,25 +47,30 @@ class ObserverMHZ19(Observer):
             'mhz19_co2': {'name': 'CO2', 'description': 'CO2 concentration from MH-Z19B', 'units': 'ppm'}
         }
         super(ObserverMHZ19, self).__init__(session = session, name = name, *args, **kwargs)
-#        mh_z19.abc_off()
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(RELAY_PIN, GPIO.OUT)
-        # GPIO.output(RELAY_PIN, GPIO.HIGH)
+        subprocess.call(stop_getty, stdout = subprocess.PIPE, shell = True)
+        self.con_serial = connect_serial()
 
     def observe(self):
-#        raw_data = subprocess.run(['python', '-m', 'mh_z19'], stdout=subprocess.PIPE)
-        try:
-            co2 = mh_z19.read().get('co2', None)
-        except:
-            self.logger.warning("Cannot read from MH-Z19B, resetting power.")
-            GPIO.output(RELAY_PIN, GPIO.HIGH)
-            time.sleep(10)
-            GPIO.output(RELAY_PIN, GPIO.LOW)
-            co2 = mh_z19.read().get('co2', None)
-        self.logger.info('CO2 = %d ppm' % (co2))
+        co2 = None
+        while 1:
+            result = self.con_serial.write(b"\xff\x01\x86\x00\x00\x00\x00\x00\x79")
+            s = self.con_serial.read(9)
+
+            if p_ver == '2':
+                if len(s) >= 4 and s[0] == "\xff" and s[1] == "\x86":
+                    co2 = ord(s[2])*256 + ord(s[3])
+                break
+            else:
+                if len(s) >= 4 and s[0] == 0xff and s[1] == 0x86:
+                    co2 = s[2]*256 + s[3]
+                break
         return {
             'mhz19_co2': co2
         }
+
+    def __del__(self):
+        print('Destructor start tty')
+        subprocess.call(start_getty, stdout = subprocess.PIPE, shell = True)
 
 if __name__ == '__main__':
     service = ObserverMHZ19(name = 'mhz19b')
